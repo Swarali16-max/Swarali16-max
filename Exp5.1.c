5c9da4814958af670eab341b2fcecb423b24f20e
Swarali16-max <swaralikarkar16@gmail.com>
2024-11-15 21:57:10 +0530

    Create Exp5.1.c

---
+++ Exp5.1.c
@@ -0,0 +1,31 @@
+/*Expt. No.:5.1
+UIN:241P066
+Title:WAP to find the factorial of a number using iterative function.
+*/
+
+#include <stdio.h>
+
+int calcIterativeFact (int x) {
+int product=1; for (int i=x; i>1; i--) {
+product=product*i;
+}
+return product;
+}
+
+int calcProduct (int x) {
+if (x==1) {
+return 1;
+}
+return x*calcProduct(x-1);
+}
+
+int main() {
+int num=5, product=1; product=calcProduct(num); printf("%d",product);
+
+return 0;
+}
+
+/*OUTPUT
+120
+=== Code Execution Successful ===
+*/
