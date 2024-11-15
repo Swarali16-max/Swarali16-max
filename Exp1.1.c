a63615a45bd03c8e67ac104cdfef4735af5cbf7f
Swarali16-max <swaralikarkar16@gmail.com>
2024-11-15 21:50:56 +0530

    Create Exp1.1.c

---
+++ Exp1.1.c
@@ -0,0 +1,29 @@
+/*Name: Swarali
+UIN:241P065
+Title:WAP to print your name,age,class,division and UIN on the screen. Use escape sequences to properly format the output. Experiment 1, task 1
+Roll Number:59 */
+#include<stdio.h>
+
+
+int main()
+{
+  printf("Name\t\t:\t Swarali\n");
+  printf("Age\t\t\t:\t18\n");
+  printf("Class\t:\tFirst Year\n");
+  printf("Div\t\t\t:\tC3\n");
+  printf("UIN\t\t\t:\t241p065\n");
+
+
+  return 0;
+
+}
+
+/*Output:
+Name            :       M Moin M Sameer Bhurani
+Age                     :       18
+Class   :       First Year
+Div                     :       C3
+UIN                     :       241p059
+
+Process returned 0 (0x0)   execution time : 0.072 s
+Press any key to continue.*/
